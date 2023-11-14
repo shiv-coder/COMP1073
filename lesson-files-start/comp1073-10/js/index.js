9456a977ba22eac31cd25a474f480e87512b3465
Shivali Dhaka <56281084+shiv-coder@users.noreply.github.com>
2023-11-14 12:13:04 -0500

    Add files via upload

---
+++ lesson-files-start/comp1073-10/js/index.js
@@ -0,0 +1,38 @@
+/* STEP 2: Bind the HEADER and the SECTION elements above to variables */
+let header = document.querySelector('header');
+let section = document.querySelector('section');
+
+		/* STEP 3: Store the URL of a JSON file in a variable */
+
+		/* STEP 4: Create a new XHR object */
+
+		/* STEP 5: Open a new request using the request() method */
+
+		/* STEP 6: Set JavaScript to accept JSON from the server */
+
+		/* STEP 7: Send the request with the send() method */
+
+		/* STEP 8: Add an event handler that listens for the onload event of the JSON file/object */
+
+		/* STEP 9: Build out the populateHeader() function */
+
+		/* STEP 10a: Assemble the showTopFlavors() function */
+
+			// STEP 10b: Bind the JSON topFlavors object to a var
+
+			// STEP 10c: Loop through the topFlavors object
+
+				// STEP 10d: build HTML elements for the content
+
+				// STEP 10e: Set the textContent property for each of the above elements (except the UL), based on the JSON content
+
+				// STEP 10f: Build a loop for the ingredients array in the JSON
+
+				// STEP 10g: Append each of the above HTML elements to the ARTICLE element
+
+				// STEP 10h: Append each complete ARTICLE element to the SECTION element
+
+
+		// This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+
+		// A special thanks to https://openclipart.org/detail/285225/ice-cream-cones for the awesome ice cream cone illustrations
