5073e6917cc5a456768631cb4086969d7e30e4e7
shiv-coder <shivi359@gmail.com>
2024-02-15 20:13:10 -0500

    Changes done to gallery.js

--- lesson-6/js/gallery.js
+++ lesson-6/js/gallery.js
@@ -1,31 +1,40 @@
 // STEP 1: Initialize and declare variables
-
-
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
 /* STEP 2: Loop 5 times to create the <img> elements */
-
-		/* STEP 3a: Create a new DOM node - an image element */
-
-		/* STEP 3b: Set the src attribute to be the path of one of the images inside the images folder, using setAttribute() */
-
-		/* Append the new image element to the thumbBar div, named in STEP 1 */
-
-		/* STEP 3c: Build event handler for each <img> */
-
-
+for (let i = 1; i < 6; i ++) {
+	/* STEP 3a: Create a new DOM node - an image element */
+	const newImage = document.createElement('img');
+	/* STEP 3b: Set the src attribute to be the path of one of the images inside the images folder, using setAttribute() */
+	newImage.setAttribute('src', `images/pic${i}.jpg`);
+	/* Append the new image element to the thumbBar div, named in STEP 1 */
+	thumbBar.appendChild(newImage);
+	/* STEP 3c: Build event handler for each <img> */
+	// newImage.addEventListener('click', function(event) {
+	// 	//console.log('Image clicked');
+	// 	// Grab the src attribute from the image that was clicked
+	// 	let imgSrc = event.target.getAttribute('src');
+	// 	//console.log(imgSrc);
+	// 	// Update the main image using the displayImage() function
+	// 	displayImage(imgSrc);
+	// });
+};
 /* STEP 4: Function to change the src of the main <img> */
-
+function displayImage(value) {
 	// Rewrite the src attribute of the .displayed-img element
-
+	displayedImage.setAttribute('src', value);
+}
 
 /* STEP 5: Event Delegation
 Instead of adding event handlers for each image, how about event delegation? Build a click handler on the parent element, and capture each target (and its attributes) from the event object */
-
+thumbBar.addEventListener('click', function(event){
 	// event.target is the element that was clicked
-
+	if (event.target && event.target.nodeName === 'IMG'){
 		// grab the src attribute of the element that was clicked
-
+		let imgSrc = event.target.getAttribute('src');
 		// change the main image
-		
+		displayImage(imgSrc);
+	}
 
 		// Lab 6 STEP G: Call the clearWayfinding() function that you built below - and enjoy the result!
 
@@ -35,7 +44,7 @@ Instead of adding event handlers for each image, how about event delegation? Bui
 
 		// Lab 6 STEP C: And then set the CSS z-index property to "10" so that the thumbnail clicked is on top of all the others
 
-
+});
 // Lab 6 STEP D: Initialize and declare a var called 'thumbImgs' using the querySelectorAll method to grab all the IMG elements inside the .thumb-bar
 
 
