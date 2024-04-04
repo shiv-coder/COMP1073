d0fb40167b73783db4670f1f3531e31802f4539b
shiv-coder <shivi359@gmail.com>
2024-04-04 16:27:25 -0400

    changes made to main.js file

--- Lesson 12 Files/Lesson 11 Files Start/js/main.js
+++ Lesson 12 Files/Lesson 11 Files Start/js/main.js
@@ -11,18 +11,70 @@ function random(min,max) {
 }
 
 // STEP 1: Create a constructor function for a ball
+function Ball(x,y,velX,velY,color,size){
+this.x = x;
+this.y =y;
+this.velX =velX;
+this.velY = velY;
+this.color = color;
+this.size = size;
+}
 
 // STEP 2: Add the draw() method to the Ball prototype
+Ball.prototype.draw = function(){
+	ctx.beginPath();
+	ctx.fillStyle=this.color;
+	ctx.arc(this.x,this.y,this.size,0,2*Math.PI);
+	ctx.fill();
 
+}
 // STEP 3: Include an update() method to the Ball prototype
-
+Ball.prototype.update = function(){
 	// Check for collision with right or left edge of screen
-	
+	if((this.x + this.size)>= width || (this.x-this.size) <=0){
+		this.velX = -(this.velX);
+	}
 	// Check for collision with top or bottom edge of screen
-	
+	if((this.y + this.size)>= height || (this.y-this.size) <=0){
+		this.velY= -(this.velY);
+	}
 	// Move the ball each time this method is called
+		this.x += this.velX;
+		this.y + this.velY;
+}
 
+// STEP 4: Create an array to contain all the balls
+let balls = [];
 
+// STEP 5: Build the animation loop
+function loop(){
+
+	ctx.fillStyle='rgba(0,0,0,0.5)';
+	ctx.fillRect(0,0,width,height);
+
+	while(balls.length < 25){
+		var ball = new Ball(
+			random(0,width),
+			random(0,height),
+			random(-7,7),
+			random(-7,7),
+			'rgb(' +random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')',
+			random(10,20)
+		);
+		balls.push(ball);
+	}
+
+	for(var i=0;i<balls.length;i++){
+		balls[i].draw();
+		balls[i].update();
+	}
+	requestAnimationFrame(loop);
+}
+	// Create up to 25 randomly colored balls, and push each one to the balls array...
+loop();
+	// For each ball object instance, draw it and update it
+	
+	// Use the requestAnimationFrame to loop
 // STEP 7: Collision detection
 
 		// If this is any ball other than balls[j]...
@@ -36,16 +88,10 @@ function random(min,max) {
 			// If balls[j] is so close to another ball that they are touching...
 			
 
-// STEP 4: Create an array to contain all the balls
 
 
-// STEP 5: Build the animation loop
 
-	// Create up to 25 randomly colored balls, and push each one to the balls array...
 
-	// For each ball object instance, draw it and update it
-	
-	// Use the requestAnimationFrame to loop
 
 
 // STEP 6: Call the animation loop() function to get things rolling
